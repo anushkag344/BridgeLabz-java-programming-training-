7ec41b104e9bac3d4d2d2bd118f7327f780c7b9c
anushkag344 <anushkag344@gmail.com>
2026-04-29 15:19:20 +0530

    second program

---
+++ Core Programming/Elementary Programming/Level 1/Program2.java
@@ -0,0 +1,9 @@
+public class Program2{
+    public static void main(String[] args){
+        int Maths=94;
+        int Physics=95;
+        int Chemistry=96;
+        double average=(Maths+Physics+Chemistry)/3.0;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
