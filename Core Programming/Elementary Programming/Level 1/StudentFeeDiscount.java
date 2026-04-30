0be2405e9d2cde47e346bab2a4065e673bb1ae3d
anushkag344 <anushkag344@gmail.com>
2026-04-30 22:18:55 +0530

    added program for student fee discount calculation

--- Core Programming/Elementary Programming/Level 1/StudentFeeDiscount.java
+++ Core Programming/Elementary Programming/Level 1/StudentFeeDiscount.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class StudentFeeDiscount {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Student Fee: ");
-        double fee = input.nextDouble();
+        try {
+            System.out.print("Enter Student Fee: ");
+            double fee = sc.nextDouble();
 
-        System.out.print("Enter Discount Percentage: ");
-        double discountPercent = input.nextDouble();
+            System.out.print("Enter Discount Percentage: ");
+            double discountPercent = sc.nextDouble();
 
-        double discount = (fee * discountPercent) / 100;
-        double finalFee = fee - discount;
+            double discount = (fee * discountPercent) / 100;
+            double finalFee = fee - discount;
 
-        System.out.println("The discount amount is INR " + discount +
-         " and final discounted fee is INR " + finalFee);
+            System.out.println("The discount amount is INR " + discount +
+                    " and final discounted fee is INR " + finalFee);
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter numeric values only.");
+        }
+
+        sc.close();
     }
 }
