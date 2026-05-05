f9c9fc77f476850ce06da5216fa48e99c5587001
anushkag344 <anushkag344@gmail.com>
2026-05-05 11:31:21 +0530

    power of a number

---
+++ Core Programming Intermediate/Control Flow/Level 2/PowerFor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PowerFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter power: ");
+        int power = sc.nextInt();
+
+        if (power < 0) {
+            System.out.println("Power should be non-negative");
+            sc.close();
+            return;
+        }
+
+        int result = 1;
+
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+
+        System.out.println("Result = " + result);
+        sc.close();
+    }
+}
