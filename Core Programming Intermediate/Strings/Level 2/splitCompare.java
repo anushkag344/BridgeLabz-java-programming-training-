ec184b19bad8bd54d1ce50230c8ca9177439675d
anushkag344 <anushkag344@gmail.com>
2026-05-06 23:46:28 +0530

    Split words without split() and compare

---
+++ Core Programming Intermediate/Strings/Level 2/splitCompare.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class splitCompare {
+    public static int findLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+            return count;
+        }
+    }
+
+    public static String[] splitWords(String text) {
+        int length = findLength(text);
+        int wordCount = 1;
+
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+
+        String[] words = new String[wordCount];
+        String word = "";
+        int index = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) != ' ') {
+                word += text.charAt(i);
+            } else {
+                words[index++] = word;
+                word = "";
+            }
+        }
+
+        words[index] = word;
+        return words;
+    }
+
+    public static boolean compareArrays(String[] arr1, String[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (!arr1[i].equals(arr2[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a text: ");
+        String text = sc.nextLine();
+
+        String[] userWords = splitWords(text);
+        String[] builtInWords = text.split(" ");
+
+        boolean result = compareArrays(userWords, builtInWords);
+
+        System.out.println("Comparison result: " + result);
+        sc.close();
+    }
+}
